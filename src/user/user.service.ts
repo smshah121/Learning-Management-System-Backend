7bdd654ffeb781f45b8c8e919f8058dd6407df05
Syed Momin Ali Shah <smshah.2003@gmail.com>
2026-07-14 23:28:54 +0500

    cs

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -5,7 +5,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import * as bcrypt from "bcrypt"
-
+import { UserRole } from 'src/common/enums/user-role.enum';
 
 
 @Injectable()
@@ -19,7 +19,8 @@ export class UserService {
     const user = this.userRepository.create({
       ...createUserDto,
       email: createUserDto.email.toLowerCase(),
-      password: hashedPassword
+      password: hashedPassword,
+      role:UserRole.STUDENT
     })
     return this.userRepository.save(user)
   }
